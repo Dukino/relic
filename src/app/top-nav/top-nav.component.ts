4576dceac24d770faee009b910defd837c103941
Dukino <bro14078@byui.edu>
2021-08-26 15:19:49 -0400

    Created under-nav for relics

--- src/app/top-nav/top-nav.component.ts
+++ src/app/top-nav/top-nav.component.ts
@@ -3,13 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-top-nav',
   templateUrl: './top-nav.component.html',
-  styleUrls: ['./top-nav.component.css']
+  styleUrls: ['./top-nav.component.css'],
 })
 export class TopNavComponent implements OnInit {
+  none: boolean = true;
+  relics: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+    removeOthers() {
+        this.none = false;
+        this.relics = false;
+    }
+
+  toggleRelics() {
+    this.removeOthers();
+    this.relics = true;
   }
 
+  toggleNone() {
+      this.removeOthers();
+      this.none = true;
+  }
 }
